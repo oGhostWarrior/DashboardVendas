b75ecc72646e703ef5b549e951f29bed3325c1c2
vianawill <willian.v.f.wv@gmail.com>
2025-07-31 21:03:35 -0300

    feat - iniciando criação de tela de login

---
+++ app/login/page.tsx
@@ -0,0 +1,26 @@
+import { LoginForm } from "@/components/login-form";
+import { GalleryVerticalEndIcon } from "lucide-react";
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm flex flex-col items-center mt-10">
+        <div className="p-4">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            <div className="text-primary-foreground flex size-6 items-center justify-center rounded-md">
+              <Image
+                src="/Icon.png"
+                alt="Logo da Axon"
+                width={32}
+                height={32}
+              />
+            </div>
+            <p className="text-xl font-semibold text-gray-900">Axon</p>
+          </a>
+        </div>
+        <LoginForm />
+      </div>
+    </div>
+  );
+}
