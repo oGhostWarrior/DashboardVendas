1d1185d87d482fe62307fdba1b5793517d24feea
Pablo Assis <pablo.phassis@gmail.com>
2025-08-12 16:22:29 -0300

    Update api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
 //Laravel
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 class ApiClient {
   private baseURL: string;
